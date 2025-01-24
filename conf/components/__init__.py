3d35d0f4daf6357c887eee37428a354ea3c2e43f
Abdulloh <abdullohnugmonovone222@gmail.com>
2025-01-24 22:03:34 +0500

    commit

--- conf/components/__init__.py
+++ conf/components/__init__.py
@@ -17,6 +17,7 @@ WSGI_APPLICATION = "conf.wsgi.application"
 
 STATIC_URL = "static/"
 STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
+STATIC_ROOT = os.path.join("BASE_DIR", "static")
 
 MEDIA_URL = "uploads/"
 MEDIA_ROOT = os.path.join(BASE_DIR, "uploads")
