a4b55cb4d1a9b9b26dc5c32e8a3ffe16aab505e4
Abdulloh <abdullohnugmonovone222@gmail.com>
2025-01-25 03:02:40 +0500

    changes

--- bot/middlewares/ban.py
+++ bot/middlewares/ban.py
@@ -1,23 +1,40 @@
-from aiogram import types
+from aiogram import types, html
 from aiogram.types import TelegramObject
+from aiogram.enums import ChatType
 from aiogram.dispatcher.middlewares.base import BaseMiddleware
 
-from typing import Dict, Any
+from typing import Any, Awaitable, Callable, Dict
 from sqlalchemy.orm import Session
 
 from bot.decorators import create_session
+from bot.misc import bot
 from db import repository as repo
 
 
-class CheckBanMiddleware(BaseMiddleware):
+class CheckBanAndChatMiddleware(BaseMiddleware):
     @create_session
-    async def __call__(self, handler, event: TelegramObject, data: Dict[str, Any], session: Session):
-        print("\n\n\n\n")
-        print(data)
-        print("\n\n\n\n")
-        print(event.dict())
-        print("\n\n\n\n")
-        # account = await repo.UsersTableRepository().get_user_account()
-        
+    async def __call__(
+        self, 
+        handler: Callable[
+            [TelegramObject, Dict[str, Any]], Awaitable[Any]
+        ],
+        event: TelegramObject, 
+        data: Dict[str, Any], 
+        session: Session
+    ) -> Any:
+        event_data = event.dict()
 
-        return await super().__call__(handler, event, data)
+        if event_data['chat']['type'] == ChatType.PRIVATE:
+            account = await repo.UsersTableRepository().get_user_account(
+                user_id = event_data['from_user']['id'], 
+                session = session
+            )
+
+            if ((not account.is_banned) if account else True) and (not event_data.get('text', 'NOT TEXT') == "/quit"):
+                return await handler(event, data)
+            else:
+                await bot.send_message(
+                    chat_id = event_data['chat']['id'], 
+                    text = html.bold("Sizning xisobingiz administrator tomonidan ban qilingan! 🚫"), 
+                    reply_to_message_id = event_data['message_id']
+                )
--- bot/utils/setup.py
+++ bot/utils/setup.py
@@ -4,11 +4,11 @@ from bot.filters.chat_type import Chat
 from bot.filters.content_type import ContentType
 from bot.filters.text import Text
 
-from bot.middlewares.ban import CheckBanMiddleware
+from bot.middlewares.ban import CheckBanAndChatMiddleware
 
 
 async def setup_middlewares(dispatcher: Dispatcher):
-    CheckBanMiddleware.register(dispatcher)
+    dispatcher.message.middleware(CheckBanAndChatMiddleware())
 
 async def setup_filters(dispatcher: Dispatcher):
     dispatcher.message.filter(Chat)
