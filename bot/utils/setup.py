33f98c43dc964152e831c895585331dea1e4c014
Abdulloh <abdullohnugmonovone222@gmail.com>
2025-01-25 02:48:27 +0500

    changes

--- bot/utils/setup.py
+++ bot/utils/setup.py
@@ -4,9 +4,11 @@ from bot.filters.chat_type import Chat
 from bot.filters.content_type import ContentType
 from bot.filters.text import Text
 
+from bot.middlewares.ban import CheckBanMiddleware
+
 
 async def setup_middlewares(dispatcher: Dispatcher):
-    ...
+    dispatcher.message.middleware(CheckBanMiddleware)
 
 async def setup_filters(dispatcher: Dispatcher):
     dispatcher.message.filter(Chat)
