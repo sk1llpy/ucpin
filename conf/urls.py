0a4b85bcf31f983d398c240641db5e4afaac2ec5
Abdulloh <abdullohnugmonovone222@gmail.com>
2025-01-26 03:00:07 +0500

    changes

--- conf/urls.py
+++ conf/urls.py
@@ -12,5 +12,9 @@ urlpatterns = [
     path('', admin.site.urls),
 ]
 
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
 handler404 = PageNotFoundView.as_view()
 
