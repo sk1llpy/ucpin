f05496161a09e8bc1af58287025cede107225d12
Abdulloh <abdullohnugmonovone222@gmail.com>
2025-01-26 03:38:21 +0500

    changes

--- apps/shop/urls.py
+++ apps/shop/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('redeemcode/add/many/', views.CreateRedeemCodeView.as_view(), name="create_redeemcode"),
+    path('admin/shop/redeemcode/add/many/', views.CreateRedeemCodeView.as_view(), name="create_redeemcode"),
 ]
--- conf/urls.py
+++ conf/urls.py
@@ -6,15 +6,14 @@ from django.conf.urls.static import static
 from apps.general.views import PageNotFoundView
 
 urlpatterns = [
-    path('shop/', include('apps.shop.urls')),
+    path('', include('apps.shop.urls')),
     
     # Admin-panel
-    path('', admin.site.urls),
+    path('admin/', admin.site.urls),
 ]
 
-if settings.DEBUG:
-    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 handler404 = PageNotFoundView.as_view()
 
