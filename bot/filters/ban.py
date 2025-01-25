acbabd6a023e0deccee6f5632b707089716aba85
Abdulloh <abdullohnugmonovone222@gmail.com>
2025-01-25 11:19:19 +0500

    change

--- bot/filters/ban.py
+++ bot/filters/ban.py
@@ -11,7 +11,7 @@ class IsBanned(Filter):
         super().__init__()
 
     @create_session
-    async def __call__(self, event: object, session: Session) -> bool:
+    async def __call__(self, event: object, session: Session, *args, **kwargs) -> bool:
         user_id = event.dict()['from']['user']
         account = await repo.UsersTableRepository().get_user_account(user_id=user_id, session=session)
 
