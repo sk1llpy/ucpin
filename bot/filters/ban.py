903dccf61c8cfa104ae7f7e13f830bad4e2593a2
Abdulloh <abdullohnugmonovone222@gmail.com>
2025-01-25 11:20:04 +0500

    changes

--- bot/filters/ban.py
+++ bot/filters/ban.py
@@ -7,12 +7,12 @@ from db import repository as repo
 from bot.decorators import create_session
 
 class IsBanned(Filter):
-    def __init__(self, *args, **kwargs):
-        super().__init__()
+    def __init__(self, is_banned: bool = True, *args, **kwargs):
+        self.is_banned = is_banned
 
     @create_session
     async def __call__(self, event: object, session: Session, *args, **kwargs) -> bool:
         user_id = event.dict()['from']['user']
         account = await repo.UsersTableRepository().get_user_account(user_id=user_id, session=session)
 
-        return account.is_banned
+        return account.is_banned == self.is_banned
